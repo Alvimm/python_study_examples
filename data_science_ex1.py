3ca98bb2380a5dcebdeb53b6d46c8fd2a54e5d22
Filipe Alvim <filipe2012alvim@gmail.com>
2022-11-11 11:39:26 -0300

    postgres test

--- data_science_ex1.py
+++
@@ -1,6 +0,0 @@
-from sklearn.datasets import load_digits
-
-digits = load_digits()
-
-print(f'Image data shape: {digits.data.shape}')
-print(f'Labeled data shape: {digits.target.shape}')
--- my_lib.py
+++
@@ -1,4 +0,0 @@
-def calculate_delta(coef_1, coef_2, coef_3):
-    delta = coef_2 * coef_2 - 4 * coef_1 * coef_3
-    return delta
-
