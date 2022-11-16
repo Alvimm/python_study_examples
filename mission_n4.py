6a9c4e0c2731a3abfd972d28a04a0edafeb0fa1c
Filipe Alvim <filipe2012alvim@gmail.com>
2022-11-16 12:53:30 -0300

    update mission 4

--- mission_n4.py
+++ mission_n4.py
@@ -3,110 +3,52 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-# ***************************** ENTRADA DE DADOS*******************************
+# ***************************** ENTRADA DOS DADOS ***************************
 file = pd.read_csv('expenses_data.csv')
 day = file['day'].values
-alimentation = file['alimentation'].values
-clothing = file['clothing'].values
-transport = file['transport'].values
+alimentation_exp = file['alimentation'].values
+clothing_exp = file['clothing'].values
+transport_exp = file['transport'].values
 
-# ***************************** SÉRIES TEMPORAIS*******************************
-
-# plt.plot(day, alimentation, '-o', color='green', linewidth=3,
-#          markerfacecolor='blue', markeredgecolor='blue', markeredgewidth=4)
-# plt.plot(day, clothing, '-o', color='yellow', linewidth=3,
-#          markerfacecolor='blue', markeredgecolor='blue', markeredgewidth=4)
-# plt.plot(day, transport, '-o', color='black', linewidth=3,
-#          markerfacecolor='blue', markeredgecolor='blue', markeredgewidth=4)
-# plt.legend(['Alimentation', 'Clothing', 'Transport'])
-# plt.xlabel('Day')
-# plt.ylabel('Expenses in BRL')
-# plt.title('Expense graphs')
+# ***************************** SÉRIES TEMPORAIS ****************************
 
 
 class Time_series():
-    def show_graphs(self, x, y, z):
+    def show_ts(self, x, y, z):
         plt.plot(day, x, '-o', color='green', linewidth=3,
-                 markerfacecolor='blue', markeredgecolor='blue', markeredgewidth=4)
+                 markerfacecolor='blue', markeredgecolor='blue', markeredgewidth=4)  # noqa
         plt.plot(day, y, '-o', color='yellow', linewidth=3,
-                 markerfacecolor='blue', markeredgecolor='blue', markeredgewidth=4)
+                 markerfacecolor='blue', markeredgecolor='blue', markeredgewidth=4)  # noqa
         plt.plot(day, z, '-o', color='black', linewidth=3,
-                 markerfacecolor='blue', markeredgecolor='blue', markeredgewidth=4)
+                 markerfacecolor='blue', markeredgecolor='blue', markeredgewidth=4)  # noqa
         plt.legend(['Alimentation', 'Clothing', 'Transport'])
         plt.xlabel('Day')
         plt.ylabel('Expenses in BRL')
         plt.title('Expense graphs')
         plt.show()
 
-
-# test = Time_series()
-# test.show_graphs(alimentation, clothing, transport)
-
-
-# ***************************** REGRESSÃO LINEAR !!!*******************************
-# ***************************************** ALIMENTATION ********************************
-regr = LinearRegression()
-day = day.reshape(-1, 1)
-regr.fit(day, alimentation)
-angular_coef = regr.coef_[0]
-linear_coef = regr.intercept_
-line = angular_coef * day + linear_coef
+# ***************************** REGRESSÃO LINEAR *****************************
 
 
 class Linear_regr():
-    def show_linear_regr(self, name):
+    def show_graphs(self, type, name_text, color):
+        global day, regr, line
+        day = np.array(day).reshape(-1, 1)
+        regr = LinearRegression().fit(day, type)
+        line = regr.coef_[0] * day + regr.intercept_
         plt.plot(day, line, label='Linear regression', c='blue')
-        plt.plot(day, name, '-o', label='alimentation - original', linewidth=3,
-                 markerfacecolor='black', markeredgecolor='black', markeredgewidth=4, c='red')
+        plt.plot(day, type, '-o', label=f'{name_text} - original', linewidth=3,
+                 markerfacecolor='black', markeredgecolor='black', markeredgewidth=4, c=f'{color}')  # noqa
         plt.xlabel('Day')
-        plt.ylabel(f'{name} expenses in BRL')
+        plt.ylabel(f'{name_text} expenses in BRL')
         plt.legend()
         plt.show()
 
 
-test = Linear_regr()
-test.show_linear_regr(alimentation)
-test.show_linear_regr(clothing)
-test.show_linear_regr(transport)
-
-# plt.plot(day, line, label='Linear regression', c='blue')
-# plt.plot(day, alimentation, '-o',
-#          label='alimentation - original', linewidth=3, markerfacecolor='black', markeredgecolor='black', markeredgewidth=4, c='red')
-# plt.xlabel('Day')
-# plt.ylabel('Alimentation expenses in BRL')
-# plt.legend()
-
-
-# ***************************************** CLOTHING ********************************
-# regr = LinearRegression()
-# day = day.reshape(-1, 1)
-# regr.fit(day, clothing)
-# angular_coef = regr.coef_[0]
-# linear_coef = regr.intercept_
-# line = angular_coef * day + linear_coef
-
-# plt.plot(day, line, label='Linear regression', c='blue')
-# plt.plot(day, clothing, '-o',
-#          label='clothing - original', linewidth=3, markerfacecolor='black', markeredgecolor='black', markeredgewidth=4, c='red')
-# plt.xlabel('Day')
-# plt.ylabel('Clothing expenses in BRL')
-# plt.legend()
-
-
-# # ***************************************** TRANSPORT ********************************
-# regr = LinearRegression()
-# day = day.reshape(-1, 1)
-# regr.fit(day, transport)
-# angular_coef = regr.coef_[0]
-# linear_coef = regr.intercept_
-# line = angular_coef * day + linear_coef
-
-# plt.plot(day, line, label='Linear regression', c='blue')
-# plt.plot(day, transport, '-o',
-#          label='transport - original', linewidth=3, markerfacecolor='black', markeredgecolor='black', markeredgewidth=4, c='red')
-# plt.xlabel('Day')
-# plt.ylabel('Transport expenses in BRL')
-# plt.legend()
-
-
-# plt.show()
+# ******************** SAÍDA(APRESENTAÇÃO) DOS DADOS ************************
+t_series = Time_series()
+t_series.show_ts(alimentation_exp, clothing_exp, transport_exp)
+l_regr = Linear_regr()
+l_regr.show_graphs(alimentation_exp, 'Alimentation', 'green')
+l_regr.show_graphs(clothing_exp, 'Clothing', 'yellow')
+l_regr.show_graphs(transport_exp, 'Transport', 'black')
