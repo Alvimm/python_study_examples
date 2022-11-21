8f1dc346d778546281cdfa63e4f07d5dd28995cf
Filipe Alvim <filipe2012alvim@gmail.com>
2022-11-21 15:23:03 -0300

    improve mission 5

--- mission_n5.py
+++ mission_n5.py
@@ -4,23 +4,22 @@ import pandas as pd
 from faker import Faker
 from wordcloud import WordCloud
 
-# *********** CREATING AND SAVING DATA FILE *******************
-
 fake = Faker()
+column_of_names = []
+column_of_numbers = []
+quantity_of_names_and_numbers = 50
+word_numbers = []
 
-names = []
-numbers = []
-
-for i in range(50):
+for i in range(quantity_of_names_and_numbers):
     students_name = fake.name()
-    student_grades = fake.random_int(1, 10)
-    names.append(students_name)
-    numbers.append(student_grades)
+    students_grade = fake.random_int(1, 10)
+    column_of_names.append(students_name)
+    column_of_numbers.append(students_grade)
 
-raw_data = {'names': names,
-            'grades': numbers}
-df = pd.DataFrame(raw_data, columns=['names', 'grades'])
-df.to_csv('data_student_grades.csv', index=False)
+creating_data = {'names': column_of_names,
+                 'grades': column_of_numbers}
+treating_data = pd.DataFrame(creating_data, columns=['names', 'grades'])
+treating_data.to_csv('data_student_grades.csv', index=False)
 file = pd.read_csv('data_student_grades.csv')
 
 # ******************** HISTOGRAM ****************
@@ -35,14 +34,13 @@ plt.show()
 
 
 # ******************** WORD CLOUD ****************
-word_numbers = []
-for i in range(50):
+for i in range(quantity_of_names_and_numbers):
     word_numbers.append(n2w.num2words(grades[i], lang='pt-BR'))
     i += 1
 
 text = " ".join(text for text in word_numbers)
-
-word_cloud = WordCloud(background_color='black', width=800, height=400).generate(text)  # noqa
+word_cloud = WordCloud(background_color='black',
+                       width=800, height=400).generate(text)
 plt.imshow(word_cloud, interpolation='bilinear')
 plt.axis('off')
-word_cloud.to_file('Words_cloud.png')
+word_cloud.to_file('words_cloud_image.png')
